7c28430ee265673adffe557b6c0a600a3a8410b9
子夜 <ziye0713@outlook.com>
2022-09-23 00:07:06 +0800

    test

---
+++ test1.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName:test1
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2022/9/230:01 Author:1523750589
+ */
+public class test1 {
+    public static void main(String args[]){
+        System.out.println("hole wold");
+
+    }
+}
