9c8b653741ea163da91402c14631646f0b3c616f
子夜 <ziye0713@outlook.com>
2022-09-23 13:17:31 +0800

    test3

--- test2.java
+++ test2.java
@@ -1,14 +1,13 @@
 /**
- * ClassName:test1
+ * ClassName:test2
  * Package:PACKAGE_NAME
  * Description:
  *
- * @Date:2022/9/230:01 Author:1523750589
+ * @Date:2022/9/2312:52 Author:1523750589
  */
-public class test1 {
+public class test2 {
     public static void main(String args[]){
         System.out.println("hole wold");
-        System.out.println("测试1");
-
-    }
+        System.out.println("测试2");
+}
 }
